12356e491570730cea23ce558291c8786dd6ac0d
JaniNtowe <84185466+JaniNtowe@users.noreply.github.com>
2024-10-12 14:07:13 -0400

    added recipe class

--- testWebScrape.js
+++ testWebScrape.js
@@ -1,36 +1,47 @@
 import puppeteer from 'puppeteer';
 
-//const pt = require('puppeteer');
+class Recipe {
+    constructor() {
+        // Ingredient array as a property of the class
+        this.ingredientsArray = [];
+    }
+
+    // Method to add an ingredient to the array
+    addIngredient(ingredient) {
+        this.ingredientsArray.push(ingredient);
+    }
+}
+
+
+let recipeLink;
+let recipe = new Recipe();
+let ingredient;
+
+
 const browser = await puppeteer.launch({headless: false});
-
-
 //const browser = await puppeteer.launch();
 const page = await browser.newPage();
 //What page to go to
-await page.goto('https://developer.chrome.com/');
+await page.goto('https://walnuts.org/recipes/?');
 //Set page size
 await page.setViewport({width: 1080, height: 1024})
+// Pause for 10 seconds, to see what's going on.
+await new Promise(r => setTimeout(r, 10000));
 
 
 
+//in theory, we go through the links, collect them, add that link to the recipe class, and add the ingredients to class too
+//array of recipes
+
+void function  addRecipeLink(recipeLink){
+//scrape through the link to find the ingredients
+    let numberOfIngredients = 0;
+    for(var i =0; i < ingredient.length; i++){
+        //use addIngredient function in recipe class to add per ingredient
+
+        }
+    }
 
 
-
-
-// //type into search box
-// await page.locator('.devsite-search-field').fill('beef');
-//    // Wait for the search results to appear
-//     await page.waitForSelector('.devsite-result-item-link', { timeout: 5000 });
-//
-// await page.locator('.devsite-result-item-link').click();
-//
-//
-// const textSelector = await page
-//     .locator('text/Customize and automate')
-//     .waitHandle();
-// const fullTitle = await textSelector?.evaluate(el => el.textContent);
-//
-// // Print the full title.
-// console.log('The title of this blog post is "%s".', fullTitle);
 
 
