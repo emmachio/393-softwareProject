2cf367642153798c1dd3706ac52f987868f851b5
emmachio <chio@STEPHENs-MacBook-Pro-9.local>
2024-11-19 15:48:02 -0500

    code to dynamically add recipe to front

--- public/recipeFront.js
+++ public/recipeFront.js
@@ -19,13 +19,7 @@
 // });
 document.addEventListener("DOMContentLoaded", () => {
     // a list of Recipes
-    const recipes = [
-        { name: "Alice", age: 25, city: "New York" },
-        { name: "Bob", age: 30, city: "Los Angeles" },
-        { name: "Charlie", age: 35, city: "Chicago" },
-        { name: "David", age: 35, city: "Chicago" },
-        { name: "Emma", age: 35, city: "Chicago" },
-        { name: "Favor", age: 35, city: "Chicago" } ];
+    const recipes = [{ name: 'Walnut Delight', image: 'https://link-to-image1.jpg' },];
 
     const outputDiv = document.getElementById('output');
 
@@ -33,7 +27,7 @@ document.addEventListener("DOMContentLoaded", () => {
         recipes.forEach(item => {
             const individualRecipe = document.createElement('div');
             individualRecipe.classList.add('recipeWidget')
-            individualRecipe.innerHTML = `Name: ${item.name}, Age: ${item.age}, City: ${item.city}`;
+            individualRecipe.innerHTML = `Name: ${item.name}, Image: ${item.image}`;
             const widgetContainer = document.querySelector('.widgetContainer');
             widgetContainer.appendChild(individualRecipe);
             console.log(individualRecipe.outerHTML);
