619aa8ed6fa1ae050a08c4f0fc00c33bf7728911
emmachio <chio@STEPHENs-MacBook-Pro-9.local>
2024-11-19 22:30:55 -0500

    testing file in progress

---
+++ testing/spec/jasmineTestingSpec.js
@@ -0,0 +1,14 @@
+describe("testing the search", () => {
+    it("check the search is right", () => {
+        // where search is the algorithm and inside of toBe is the expected value
+        expect(2).toBe(2);
+    });
+    it("check the search is right", () => {
+        // where search is the algorithm and inside of toBe is the expected value
+        expect(2).toBe(3);
+    });
+    it("check the search is right", () => {
+        // where search is the algorithm and inside of toBe is the expected value
+        expect(2).toBe(2);
+    });
+});
