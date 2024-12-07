f86c01174b3752365406f0e91c9b0e2f043efc60
JaniNtowe <84185466+JaniNtowe@users.noreply.github.com>
2024-12-06 23:05:49 -0500

    cleared tbd

--- TBDFile.js
+++ TBDFile.js
@@ -18,37 +18,7 @@ class Recipe {
 }
 
 
-// async function findRecipesByIngredients(ingredientsArray) {
-//     try {
-//         // Read the JSON file asynchronously
-//         const data = await fs.readFile('AllRecipes.json', 'utf8')
-//         const jsonData = JSON.parse(data);
-//
-//         const matchingRecipes = [];
-//
-//         // Iterate through each recipe in the JSON data
-//         jsonData.forEach(item => {
-//             // Check if the ingredientsArray items are partially present in the recipe's ingredientsArray
-//             const containsAllIngredients = ingredientsArray.every(inputIngredient =>
-//                 item.ingredientsArray.some(recipeIngredient =>
-//                     recipeIngredient.toLowerCase().includes(inputIngredient.toLowerCase())
-//                 )
-//             );
-//
-//             if (containsAllIngredients) {
-//                 const recipe = new Recipe(item.recipeName);
-//                 recipe.setLink(item.recipeLink);
-//                 item.ingredientsArray.forEach(ingredient => recipe.addIngredient(ingredient));
-//                 matchingRecipes.push(recipe);
-//             }
-//         });
-//
-//         return matchingRecipes; // Resolve with matching recipes
-//     } catch (error) {
-//         console.error('Error in findRecipesByIngredients:', error);
-//         throw error; // Reject the promise with the error
-//     }
-// }
+
 
 export async function findRecipesByIngredientsNew(ingredientsArray, pathway ='./AllRecipes.json') {
     try {
@@ -115,21 +85,6 @@ export async function resultJSON(exampleIngredientsNew, pathway){
     }
 }
 
-const exampleIngredients = [
-    "2 (14.75 ounce) cans salmon, drained and flaked",
-    "¾ cup Italian-seasoned panko (Japanese bread crumbs)",
-    "½ cup minced fresh parsley",
-    "2 eggs, beaten",
-    "2 green onions, chopped",
-    "3 tablespoons Worcestershire sauce",
-    "3 tablespoons grated Parmesan cheese",
-    "2 tablespoons Dijon mustard",
-    "2 tablespoons creamy salad dressing (such as Miracle Whip®)",
-    "2 teaspoons seafood seasoning (such as Old Bay®)",
-    "1 ½ teaspoons garlic powder",
-    "1 ½ teaspoons ground black pepper",
-    "1 tablespoon olive oil, or as needed, divided"
-];
 const exampleIngredientsNew = [
     "salmon",
     "panko",
@@ -162,62 +117,4 @@ const exampleIngredientsNew = [
 ]
 
 
-
-
-// findRecipesByIngredients(exampleIngredientsNew)
-//     .then(matchingRecipes => {
-//         if (matchingRecipes.length > 0) {
-//             console.log('Matching Recipes:');
-//             matchingRecipes.forEach(recipe => {
-//                 console.log(`Recipe Name: ${recipe.name}`);
-//                 console.log(`Recipe Link: ${recipe.link}`);
-//                 console.log('Ingredients:', recipe.ingredientsArray);
-//                 console.log('----------------------');
-//             });
-//         } else {
-//             console.log('No matching recipes found.');
-//         }
-//     })
-//     .catch(err => console.error('Error:', err));
-//
-// findRecipesByIngredientsNew(exampleIngredientsNew)
-//     .then(matchingRecipes => {
-//         if (matchingRecipes.length > 0) {
-//             console.log('Matching Recipes New:');
-//             matchingRecipes.forEach(recipe => {
-//                 console.log(`Recipe Name: ${recipe.name}`);
-//                 console.log(`Recipe Link: ${recipe.link}`);
-//                 console.log('Ingredients:', recipe.ingredientsArray);
-//                 console.log("Recipe Image Link", recipe.imgSrc);
-//                 console.log('----------------------');
-//             });
-//         } else {
-//             console.log('No matching recipes found.');
-//         }
-//     })
-//     .catch(err => console.error('Error:', err));
-
-
-// findRecipesByIngredients(exampleIngredientsNew)
-//     .then(matchingRecipes => {
-//         if (matchingRecipes.length > 0) {
-//             console.log('Matching Recipes:');
-//             matchingRecipes.forEach(recipe => {
-//                 console.log(`Recipe Name: ${recipe.name}`);
-//                 console.log(`Recipe Link: ${recipe.link}`);
-//                 console.log('Ingredients:', recipe.ingredientsArray);
-//                 console.log('----------------------');
-//             });
-//         } else {
-//             console.log('No matching recipes found.');
-//         }
-//     })
-//     .catch(err => console.error('Error:', err));
-
-// (async () => {
-//     const ingredients = ["salmon", "panko", "eggs"];
-// const recipes = await findRecipesByIngredientsNew(exampleIngredientsNew);
-// console.log(recipes); // Logs the array of recipes
-// })();
-// Emma(exampleIngredientsNew)
 resultJSON(exampleIngredientsNew, './AllRecipes.json');
