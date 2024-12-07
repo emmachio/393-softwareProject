a6c4c7612329012bd13bf3063ee5f6c45f2200b4
ellycho08 <eycho1134@gmail.com>
2024-12-06 21:16:44 -0500

    Saving changes before revert

--- scrape.js
+++ scrape.js
@@ -1,22 +1,21 @@
 import puppeteer from "puppeteer";
-//implement auto testing in github
+
 const scrape = async () => {
     const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
 
-    const url = 'https://walnuts.org/recipes/?';
+    const url = 'https://books.toscrape.com';
 
-    await page.goto(url, {waitUntil: 'domcontentloaded', timeout: 0});
+    await page.goto(url);
 
-    const title = await page.title();
-    console.log('PAGE TITLE: ' + title);
+    //const title = await page.title();
+    //console.log(title);
 
     const recipeNames = await page.evaluate(() => {
         const elements = document.querySelectorAll('h3 a');  // Select the <a> tags inside <h3>
         return Array.from(elements).map(el => el.textContent.trim());  // Extract only the text content
       });
-    console.log('ALL RECIPE NAMES: ', recipeNames);
-
+    
     //await browser.close();
 };
 
