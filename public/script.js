2b6e8bf2db54bdef73dd095eee5ffe257919e511
ellycho08 <eycho1134@gmail.com>
2024-12-06 20:57:37 -0500

    script update

--- public/script.js
+++ public/script.js
@@ -1,32 +1,7 @@
-import {resultJSON} from '../TBDFile.js';
-document.getElementById('searchForm').addEventListener('submit', function(event) {
-  event.preventDefault();
 
-  const searchBar = document.getElementById('ingredients');
-  const searchList = document.getElementById('searchList');
-  const query = searchBar.value.trim();
-
-  if (query) {
-      // Retrieve existing history from local storage or initialize an empty array
-      let searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
-      
-      // Add the new search query to the history
-      searchHistory.push(query);
-
-      // Save updated history back to local storage
-      localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
-
-      // Clear the search input
-      searchBar.value = '';
-
-      // Optional: Log a confirmation message
-      console.log("Search saved to history.");
-      console.log(searchList);
-  }
-
-  // After submitting, also trigger the recipe filtering function
+// After submitting, also trigger the recipe filtering function
   //filterRecipesByIngredients(); 
-});
+
 
 // Import the function from recipes.js
 //import { findRecipesByIngredientsJani } from './allRecipeWebScrape.js';
